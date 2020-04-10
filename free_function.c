0cc6b3e22423e36b4011de14ce546f72ae1b5e1b
RicardoBarretoR <richardbarrodriguez@hotmail.com>
2020-04-10 00:32:53 +0000

    adding file to frees functions

---
+++ free_function.c
@@ -0,0 +1,15 @@
+#include "simple_shell"
+/**
+ *free_arraybid - frees a array bidimentional
+ *@array: double pointer that point at address of memory of a array
+ */
+void free_arraybid(char **array)
+{
+        int i;
+
+        for (i = 0; array[i] != NULL; i++)
+                free(array[i]);
+
+        free(array);
+}
+
